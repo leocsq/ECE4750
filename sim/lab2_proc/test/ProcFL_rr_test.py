807839f14011e478d4dd18afc297968fe302e72f
fq26 <fq26@cornell.edu>
2016-09-24 18:11:18 -0400

    sra FL_test success coded by feng qi

--- sim/lab2_proc/test/ProcFL_rr_test.py
+++ sim/lab2_proc/test/ProcFL_rr_test.py
@@ -190,7 +190,13 @@ import inst_sra
 
 @pytest.mark.parametrize( "name,test", [
   asm_test( inst_sra.gen_basic_test     ) ,
-
+  asm_test( inst_sra.gen_dest_dep_test  ) ,
+  asm_test( inst_sra.gen_src0_dep_test  ) ,
+  asm_test( inst_sra.gen_src1_dep_test  ) ,
+  asm_test( inst_sra.gen_srcs_dep_test  ) ,
+  asm_test( inst_sra.gen_srcs_dest_test ) ,
+  asm_test( inst_sra.gen_value_test     ) ,
+  asm_test( inst_sra.gen_random_test    ) ,
   # ''' LAB TASK '''''''''''''''''''''''''''''''''''''''''''''''''''''''''
   # Add more rows to the test case table to test more complicated
   # scenarios.
--- sim/lab2_proc/test/inst_sra.py
+++ sim/lab2_proc/test/inst_sra.py
@@ -49,3 +49,116 @@ def gen_basic_test():
 # ''' LAB TASK ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 # Define additional directed and random test cases.
 # '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+#-------------------------------------------------------------------------
+# gen_dest_dep_test
+#-------------------------------------------------------------------------
+
+def gen_dest_dep_test():
+  return [
+    gen_rr_dest_dep_test( 5, "sra",   1,  1,  0 ),
+    gen_rr_dest_dep_test( 4, "sra",   2,  1,  1 ),
+    gen_rr_dest_dep_test( 3, "sra",  -3,  1, -2 ),
+    gen_rr_dest_dep_test( 2, "sra",  -4,  2, -1 ),
+    gen_rr_dest_dep_test( 1, "sra",   5,  2,  1 ),
+    gen_rr_dest_dep_test( 0, "sra",  -6,  2, -2 ),
+  ]
+#-------------------------------------------------------------------------
+# gen_src0_dep_test
+#-------------------------------------------------------------------------
+
+def gen_src0_dep_test():
+  return [
+    gen_rr_src0_dep_test( 5, "sra",   7,  1,   3 ),
+    gen_rr_src0_dep_test( 4, "sra",   8,  1,   4 ),
+    gen_rr_src0_dep_test( 3, "sra",  -9,  1,  -5 ),
+    gen_rr_src0_dep_test( 2, "sra", -10,  2,  -3 ),
+    gen_rr_src0_dep_test( 1, "sra",  11,  2,   2 ),
+    gen_rr_src0_dep_test( 0, "sra", -12,  2,  -3 ),
+  ]
+#-------------------------------------------------------------------------
+# gen_src1_dep_test
+#-------------------------------------------------------------------------
+
+def gen_src1_dep_test():
+  return [
+    gen_rr_src1_dep_test( 5, "sra",  13,  1,   6 ),
+    gen_rr_src1_dep_test( 4, "sra",  14,  1,   7 ),
+    gen_rr_src1_dep_test( 3, "sra", -15,  1,  -8 ),
+    gen_rr_src1_dep_test( 2, "sra", -16,  2,  -4 ),
+    gen_rr_src1_dep_test( 1, "sra",  17,  2,   4 ),
+    gen_rr_src1_dep_test( 0, "sra", -18,  2,  -5 ),
+  ]
+#-------------------------------------------------------------------------
+# gen_srcs_dep_test
+#-------------------------------------------------------------------------
+
+def gen_srcs_dep_test():
+  return [
+    gen_rr_srcs_dep_test( 5, "sra",  19,  1,   9 ),
+    gen_rr_srcs_dep_test( 4, "sra",  20,  1,  10 ),
+    gen_rr_srcs_dep_test( 3, "sra", -21,  1, -11 ),
+    gen_rr_srcs_dep_test( 2, "sra", -22,  2,  -6 ),
+    gen_rr_srcs_dep_test( 1, "sra",  23,  2,   5 ),
+    gen_rr_srcs_dep_test( 0, "sra", -24,  2,  -6 ),
+  ]
+
+#-------------------------------------------------------------------------
+# gen_srcs_dest_test
+#-------------------------------------------------------------------------
+
+def gen_srcs_dest_test():
+  return [
+    gen_rr_src0_eq_dest_test( "sra",  25, 1, 12 ),
+    gen_rr_src1_eq_dest_test( "sra", -26, 1,-13 ),
+    gen_rr_src0_eq_src1_test( "sra", 27, 0 ),
+    gen_rr_srcs_eq_dest_test( "sra", 28, 0 ),
+  ]
+
+#-------------------------------------------------------------------------
+# gen_value_test // imcomplete
+#-------------------------------------------------------------------------
+
+def gen_value_test():
+  return [
+
+    gen_rr_value_test( "sra", 0x00000000, 0x00000000, 0x00000000 ),
+    gen_rr_value_test( "sra", 0x00000001, 0x00000001, 0x00000000 ),
+    gen_rr_value_test( "sra", 0x00000007, 0x00000003, 0x00000000 ),
+    
+    gen_rr_value_test( "sra", 0x00010000, 0x00000004, 0x00001000 ),
+    gen_rr_value_test( "sra", 0x80000000, 0x00000005, 0xfc000000 ),
+    gen_rr_value_test( "sra", 0x89000000, 0x00000008, 0xff890000 ),
+
+    #gen_rr_value_test( "sra", 0x00000000, 0x00007fff, 0x00000001 ),
+    #gen_rr_value_test( "sra", 0x7fffffff, 0x00000000, 0x00000000 ),
+    #gen_rr_value_test( "sra", 0x7fffffff, 0x00007fff, 0x00000000 ),
+
+    #gen_rr_value_test( "sra", 0x80000000, 0x00007fff, 0xffffffff ),
+    #gen_rr_value_test( "sra", 0x7fffffff, 0xffff8000, 0x00000001 ), 
+       
+    gen_rr_value_test( "sra", 0x00000000, 0xffffffff, 0x00000000 ),
+    gen_rr_value_test( "sra", 0xfffffffb, 0x00000001, 0xfffffffd ),
+    gen_rr_value_test( "sra", 0xffffffff, 0xffffffff, 0xffffffff ),    
+  ]
+
+#-------------------------------------------------------------------------
+# gen_random_test
+#-------------------------------------------------------------------------
+
+def gen_random_test():
+  asm_code = []
+  for i in xrange(100):
+    #src0 = Bits( 32, random.randint(0,0x7fffffff) )
+    #src1 = Bits( 32, random.randint(0,0x7fffffff) )
+    #dest = src0>>src1
+    #src0 = random.randint(0,2147483647)
+    
+    src0 = random.randint(0,4294967295)
+    src1 = random.randint(0,4294967295)
+    #src1 = random.randint(0,2147483647)    
+    dest = src0 >> src1
+    #asm_code.append( gen_rr_value_test( "sra", src0.uint(), src1.uint(), dest.uint() ) )
+
+    #asm_code.append( gen_rr_value_test( "sra", src0, src1, dest ) )
+  return asm_code
+
